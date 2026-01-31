09379b3db8620157217c5ea7d060d9784c5f07ee
AlexandreKanha <alexandrelskanha@outlook.com>
2026-01-30 23:35:38 -0300

    chore: initialize Spring Boot project structure

---
+++ backend/taskincidentmanager/src/main/java/com/alexandre/taskmanager/TaskIncidentManagerApplication.java
@@ -0,0 +1,13 @@
+package com.alexandre.taskmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskIncidentManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskIncidentManagerApplication.class, args);
+	}
+
+}
